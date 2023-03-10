91ed51182f2457d49dafb34189c0ac4fd3cd6480
Joel Brigida <joel@drtechreps.com>
2023-03-10 11:15:15 -0500

    Combine ai.py & game.py into tictactoe.py

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 # Window Size:
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 1200
+HEIGHT = 1200
 
 # Board Parameters:
 ROWS = 3
 COLS = 3
 SQSIZE = WIDTH // COLS
-LINE_WIDTH = 15
+LINE_WIDTH = 25
 
 # Board Parameter Colors:
 BG_COLOR = (28, 170, 156) # RGB Format Colors 
@@ -15,9 +15,9 @@ LINE_COLOR = (23, 145, 135)
 # '0' Board Character Parameters:
 CIRC_COLOR = (239, 231, 200)
 RADIUS = SQSIZE // 4
-CIRC_WIDTH = 15
+CIRC_WIDTH = 35
 
 # 'X' Board Character Parameters:
 X_COLOR = (67, 67, 67)
-X_WIDTH = 20
-OFFSET = 55
+X_WIDTH = 30
+OFFSET = 75
